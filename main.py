3159d550b93a5b637f9fc795ddfa86602495e260
BuildTools <unconfigured@null.spigotmc.org>
2024-02-03 22:38:33 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from datetime import datetime
+import yt_modules
+from starlette.middleware.cors import CORSMiddleware  # 追加
+
+import uvicorn
+
+import os
+
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# CORSを回避するために追加（今回の肝）
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,  # 追記により追加
+    allow_methods=["*"],  # 追記により追加
+    allow_headers=["*"],  # 追記により追加
+)
+
+
+@app.get("/ydl-back")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/ydl-back/yt_donwload/{url:path}")
+def read_item(url: str):
+    yt_modules.download_yt(url)
+
+
+@app.get("/ydl-back/get_file/{filename:path}")
+async def get_file(filename: str):
+    current = Path()
+    file_path = current / "files" / filename
+
+    response = FileResponse(path=file_path, filename=filename)
+
+    return response
+
+
+@app.get("/ydl-back/delete_file/{filename:path}")
+async def get_file(filename: str):
+    current = Path()
+    file_path = current / "files" / filename
+
+    # print(file_path)
+
+    files = os.remove(file_path)
+
+
+@app.get("/ydl-back/file_info")
+async def get_file():
+    files = os.listdir("files")
+    result_list = []
+    movie_no = 1
+    for file in files:
+        yt_modules.rename_file("files/" + file)
+        movie_info = {"title": file, "no": movie_no}
+        result_list.append(movie_info)
+        movie_no = movie_no + 1
+
+    print(result_list)
+    return result_list
---
+++ yt_modules.py
@@ -0,0 +1,39 @@
+from yt_dlp import YoutubeDL
+
+from pathlib import Path
+from datetime import datetime
+
+from fastapi.responses import FileResponse
+import os
+
+
+def read_youtube_info(movie_url: str):
+    ydl_opts = {
+        "writeautomaticsub": "False",
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        res = ydl.extract_info(movie_url, download=False)
+
+
+def download_yt(movie_url: str):
+    file_name = "%(title)s.%(ext)s"
+
+    file_path = "files/{}".format(file_name)
+
+    # 動画用
+    # ydl_opts = {"format": "best", "outtmpl": file_path}
+    # 音声用
+    ydl_opts = {"format": "bestaudio/best", "outtmpl": file_path}
+
+    print("***********開始*******************")
+    print(movie_url)
+    with YoutubeDL(ydl_opts) as ydl:
+        result = ydl.download([movie_url])
+
+
+def rename_file(file_name: str):
+    after_name = file_name.replace(" ", "")
+    after_name = after_name.replace(".webm", ".mp3")
+
+    os.rename(file_name, after_name)
+    print(after_name)
